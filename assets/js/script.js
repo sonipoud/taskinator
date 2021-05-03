31111b8c847da18b9c10b6eacce7aec219784994
Sonika <sonikapoudyal5@gmail.com>
2021-05-03 18:52:17 -0400

    added a form element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 var buttonEl = document.querySelector(" #save-task ");
-var tasksTODoEl = document.querySelector(" #tasks-to-do ");
+var tasksToDoEl = document.querySelector(" #tasks-to-do ");
 
 var createTaskHandler = function() {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-  };
+  }
   
 buttonEl.addEventListener("click", createTaskHandler);
 
